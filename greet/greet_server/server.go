7d804ffadbcb6c7d4421c38260a5053a72c3b123
Naufal Ziyad <naufal.ziyad@detik.com>
2019-04-06 15:26:58 +0700

    add func greet in greetserver

--- greet/greet_server/server.go
+++ greet/greet_server/server.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
 	"net"
@@ -10,6 +11,16 @@ import (
 )
 
 type server struct{}
+
+func (*server) Greet(ctx context.Context, req *greetpb.GreetRequest) (*greetpb.GreetResponse, error) {
+	firstName := req.GetGreeting().GetFirstName()
+	result := "Hallo " + firstName
+	res := &greetpb.GreetResponse{
+		Result: result,
+	}
+	return res, nil
+
+}
 
 func main() {
 	fmt.Println("Hallo my course proto")
